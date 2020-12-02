efd32bf9cfedbf2714f64cc0876e4d0eec49f90e
cami-jara <camilati.jarase@gmail.com>
2020-12-02 16:52:41 -0300

    Evaluacion de Ghibli Api

---
+++ ghibli/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { FilmComponent } from './film/film.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+{​​​​​​​ path: 'home', component: HomeComponent}​​​​​​​,
+{​​​​​​​ path: 'about', component: AboutComponent}​​​​​​​,
+{​​​​​​​ path: 'film', component: FilmComponent }​​​​​​​,
+{​​​​​​​ path: '', redirectTo: '/home', pathMatch: 'full' }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
